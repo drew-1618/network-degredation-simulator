84c08b57b5958defb4dc3cf44df53eb4ecb853d2
Andrew Johnson <andrewjohnson11235@gmail.com>
2025-11-19 15:13:17 -0600

    Add degredation class

---
+++ degradation_engine.py
@@ -0,0 +1,49 @@
+import time
+import random
+from collections import deque
+
+class DegradationEngine:
+    def __init__(self):
+        # queue to hold inputs
+        self.action_queue = deque()
+        self.latency = 0
+        self.loss_percent = 0.0
+
+        self.packets_sent = 0
+        self.packets_lost = 0
+
+    def set_parameters(self, latency, loss_percent):
+        """Update degradation params from slider values"""
+        self.latency = latency
+        self.loss_percent = loss_percent
+
+    def queue_input(self, target_paddle, data):
+        """Receive input and queue it if it passes loss check"""
+        self.packets_sent += 1
+
+        # check for packet loss
+        if random.random() * 100 < loss_percent:
+            self.packets_lost += 1
+            return None  # drop packet
+        
+        # apply latency
+        delay_seconds = self.latency / 1000
+        time_due = time.time() + delay_seconds
+
+        # queue action
+        action = {
+            'target' : target_paddle,
+            'data' : data,  # direction (-1 or 1)
+            'time_due' : time_due
+        }
+        self.action_queue.append(action)
+
+    def get_due_actions(self):
+        """Return list of all past due actions. Remove them from queue"""
+        released_actions = []
+        current_time = time.time()
+
+        # pop oldest action if its due
+        while self.action_queue and self.action_queue[0]['time_due'] <= current_time:
+            released_actions.append(self.action_queue.popleft())
+        return released_actions
