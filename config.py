887b1ba18cde5695dad0190b43fd0175e1200db7
Andrew Johnson <andrewjohnson11235@gmail.com>
2025-11-19 08:59:44 -0600

    Add config constants for game params

---
+++ config.py
@@ -0,0 +1,12 @@
+# game constants
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# component constants
+PADDLE_WIDTH, PADDLE_HEIGHT = 15, 100
+BALL_SIZE = 15
+BALL_SPEED = 4
