510e64087df8fca8b607c94609c05e7e9349657f
Andrew Johnson <andrewjohnson11235@gmail.com>
2025-11-20 14:33:58 -0600

    Feat: Allow space bar to also start/pause/play again

--- components/button.py
+++ components/button.py
@@ -8,10 +8,18 @@ class Button:
         self.color = color
         self.font = pygame.font.Font(None, 30)
         self.is_clicked = False
+        self.is_hovered = False
 
     def draw(self, screen):
         """Draw button and center text"""
-        pygame.draw.rect(screen, self.color, self.rect, border_radius=5)
+        # check hover and set color
+        current_color = self.color
+        if self.is_hovered:
+            if self.color == BLUE:
+                current_color = DARKER_BLUE
+            else:
+                current_color = DARK_GREEN
+        pygame.draw.rect(screen, current_color, self.rect, border_radius=5)
 
         text = self.font.render(self.text, True, WHITE)
         text_rect = text.get_rect(center=self.rect.center)
@@ -19,9 +27,13 @@ class Button:
 
     def handle_click(self, event):
         """Check mouse for clicks on the button surface"""
-        # enter key to start, pause, or play again
+        # check hover state
+        if event.type == pygame.MOUSEMOTION:
+            self.is_hovered = self.rect.collidepoint(event.pos)
+
+        # enter key or space key to start, pause, or play again
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
                 self.is_clicked = True
                 return True
         # mouse click button to start, pause, or play again
